73760e06a3942eb20ea61f2ac5a015b1c9c879e0
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2024-05-29 01:35:02 +0530

    Data collection over

---
+++ src/projectData.js
@@ -0,0 +1,66 @@
+// flappy
+// 2048
+// procedural gen
+// 30 pieces
+
+// shakespeare
+// https://flamboyant-noyce-415776.netlify.app/
+// sineway
+// https://epic-lamarr-9b2fd1.netlify.app/
+
+// medium series
+// RN Animation series
+
+
+// src/data.js
+const projects = [
+    {
+      name: "Flappy Bird via NEAT",
+      link: "https://romantic-bhaskara-dc0b13.netlify.app/",
+      pic: "images/flappy.png",
+      description: "This is a brief description of Flappy Bird."
+    },
+    {
+      name: "2048 via Heuristics",
+      link: "https://inspiring-easley-8e7bf9.netlify.app/",
+      pic: "images/2048.png",
+      description: "This is a brief description of Flappy Bird."
+    },
+    {
+      name: "Procedural Content Generation via Adversarial Reinforcement Learning",
+      link: "https://proceduralcontentgeneration.netlify.app/",
+      pic: "images/procedural1.png",
+      description: "This is a brief description of Flappy Bird."
+    },
+    {
+      name: "30 pieces",
+      link: "https://jerryjohnthomas.github.io/30pieces",
+      pic: "images/30pieces1.png",
+      description: "This is a brief description of Flappy Bird."
+    },
+    {
+      name: "Word Searcher via Genetic Algo",
+      link: "https://flamboyant-noyce-415776.netlify.app/",
+      pic: "images/Shakespear1.png",
+      description: "This is a brief description of Flappy Bird."
+    },
+    {
+      name: "Sine Wave simulation ",
+      link: "https://flamboyant-noyce-415776.netlify.app/",
+      pic: "images/Shakespear1.png",
+      description: "This is a brief description of Flappy Bird."
+    },
+    {
+      name: "React Native Animation Series ",
+      link: "https://medium.com/@jerryjohnthomas/list/react-native-animations-bebaf1e64d4c",
+      pic: "images/Shakespear1.png",
+      description: "This is a brief description of Flappy Bird."
+    },
+
+    https://medium.com/@jerryjohnthomas/list/reinforcement-learning-series-season-1-6fc57525318e
+
+    // Add more projects as needed
+  ];
+  
+  export default projects;
+  
