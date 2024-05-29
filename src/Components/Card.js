df43887b5adda9042d63a2d975d307406f25f699
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2024-05-30 02:21:08 +0530

    Card Finished

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,32 @@
 import React from "react";
 import "./Card.css";
-function Card({ data , id}) {
+import { IoMdArrowForward } from "react-icons/io";
+import { PiArrowRightThin } from "react-icons/pi";
+import { GoArrowUpRight } from "react-icons/go";
+import { Link } from "react-router-dom";
+
+function Card({ data, id }) {
     return (
         <div className="card">
-            <div className="card-image-container">
-                <img src={data.pic} alt={data.title} className="card-image" />
-                <div className="card-title">{data.oneWord}</div>
-            </div>
-            <p className="card-content">{data.descriptioncontent}</p>
+            <Link to={`project/${data.goto}`}>
+                <div className="card-image-container">
+                    <div className="tags_container">
+                        {data.tags.map((tag, tag_id) => {
+                            return (
+                                <div className="tag" key={tag_id + "a" + id}>
+                                    {tag}
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div className="circle_arrow">
+                        <GoArrowUpRight size={25} />
+                    </div>
+                    <img src={data.pic} alt={data.title} className="card-image" />
+                    <div className="card-title">{data.oneWord}</div>
+                </div>
+                <p className="card-content">{data.descriptioncontent}</p>
+            </Link>
         </div>
     );
 }
--- src/projectsData.js
+++ src/projectsData.js
@@ -19,7 +19,9 @@ const projectsData = [
       title: "Flappy Bird via NEAT",
       link: "https://romantic-bhaskara-dc0b13.netlify.app/",
       pic: "images/flappy.png",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto: "flappybird",
+      tags: ["cool","AI"],
     },
     {
       oneWord: "AI 2048",
@@ -27,50 +29,65 @@ const projectsData = [
       link: "https://inspiring-easley-8e7bf9.netlify.app/",
       // pic: "images/2048.png",
       pic: "images/2048_hd_gray.png",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto: "2048",
+      tags: ["cool","AI"],
     },
     {
       oneWord: "Adversarial RL",
       title: "Procedural Content Generation via Adversarial Reinforcement Learning",
       link: "https://proceduralcontentgeneration.netlify.app/",
       pic: "images/procedural1.png",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto: "advRL",
+      tags: ["cool","AI"],
+
     },
     {
       oneWord: "30 pieces",
       title: "30 pieces",
       link: "https://jerryjohnthomas.github.io/30pieces",
       pic: "images/30pieces1.png",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto:"30pieces",
+      tags: ["cool","AI"],
     },
     {
       oneWord: "Searcher",
       title: "Word Searcher via Genetic Algo",
       link: "https://flamboyant-noyce-415776.netlify.app/",
       pic: "images/Shakespear1.png",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto:"searcher",
+      tags: ["cool","AI"],
     },
     {
       oneWord: "Sine Wave ",
       title: "Sine Wave simulation ",
       link: "https://flamboyant-noyce-415776.netlify.app/",
       pic: "images/sine4_blueinverted.png",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto:"sine",
+      tags: ["cool","AI"],
     },
     {
       oneWord: "RN Animation",
       title: "React Native Animation Series - Medium",
       link: "https://medium.com/@jerryjohnthomas/list/react-native-animations-bebaf1e64d4c",
       pic: "images/RN_Animations.jpeg",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto:"RNAnimations",
+      tags: ["cool","AI"],
     },
     {
       oneWord: "RL Series",
       title: "Reinforcement Learning Series - Medium ",
       link: "https://medium.com/@jerryjohnthomas/list/reinforcement-learning-series-season-1-6fc57525318e",
       pic: "images/RL.jpeg",
-      // pic: "images/Med_RL_series1.png",
-      description: "This is a brief description of Flappy Bird."
+      description: "This is a brief description of Flappy Bird.",
+      goto:"RLseries",
+      tags: ["cool","AI"],
+
     },
 
   ];
