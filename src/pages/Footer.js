8ba52a76fd955c02ba33e65cca52aab00b1844f8
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2024-05-31 02:23:11 +0530

    connect with me done

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,29 +4,16 @@ import { GoArrowUpRight } from "react-icons/go";
 import HoverText from "./HoverText"
 
 function Footer() {
+    
     return (
         <div className="footer_container">
             <div className="footer_left">
                 <div className="footer_title">Connect with me</div>
                 <div className="connect_links ">
-                    <div className="hover-text1">
-                        Linkedin
-                        <GoArrowUpRight />
-                    </div>
-                    <div className="hover-text2">
-                        Github
-                        <GoArrowUpRight />
-                    </div>
-                    <div className="hover-text3">
-                        Medium
-                        <GoArrowUpRight />
-                    </div>
-                    <div className="hover-text4">
-                        Gmail
-                        <GoArrowUpRight />
-                    </div>
-                    <HoverText text="asd" hoverText="asd"></HoverText>
-                    <HoverText text="asd1" hoverText="asd1"></HoverText>
+                    <HoverText text="Linkedin" hoverText="L7nk3diN" to="https:linkedin.com/in/jerry-john-thomas"></HoverText>
+                    <HoverText text="Github" hoverText="G7Th8B" to="https://github.com/JerryJohnThomas/"></HoverText>
+                    <HoverText text="Medium" hoverText="M3d1uW"to="https://medium.com/@jerryjohnthomas"></HoverText>
+                    <HoverText text="Gmail" hoverText="gWaiL" mail="jerryjohnthomascareer@gmail.com"></HoverText>
                 </div>
             </div>
             <div className="footer_right"></div>
--- src/pages/HoverText.js
+++ src/pages/HoverText.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './HoverText.css';
-import { GoArrowUpRight } from 'react-icons/go';
+import React from "react";
+import "./HoverText.css";
+import { GoArrowUpRight } from "react-icons/go";
+import { Link } from "react-router-dom";
 
-function HoverText({ text, hoverText }) {
+function HoverText({ text, hoverText, to, mail }) {
+    const handleMailTo = () => {
+        window.location.href = `mailto:${mail}`;
+    };
     return (
         <div className="hover-text-container">
-            <span className="hover-text" data-hover={hoverText}>
-                {text}
-                <GoArrowUpRight />
-            </span>
+            {to != null ? (
+                <Link to={to} target="_blank">
+                    <span className="hover-text" data-hover={hoverText}>
+                        {text}
+                    </span>
+                    <GoArrowUpRight />
+                </Link>
+            ) : (
+                <div onClick={handleMailTo} target="_blank">
+                    <span className="hover-text" data-hover={hoverText}>
+                        {text}
+                    </span>
+                    <GoArrowUpRight />
+                </div>
+            )}
         </div>
     );
 }
