67d4415a0aa8c0879de7dfad378b942ea1869124
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2024-05-31 03:08:41 +0530

    Update Footer.js LinkedIn link

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
             <div className="footer_left">
                 <div className="footer_title">Connect with me</div>
                 <div className="connect_links ">
-                    <HoverText text="Linkedin" hoverText="L7nk3diN" to="https:linkedin.com/in/jerry-john-thomas"></HoverText>
+                    <HoverText text="Linkedin" hoverText="L7nk3diN" to="https://linkedin.com/in/jerry-john-thomas"></HoverText>
                     <HoverText text="Github" hoverText="G7Th8B" to="https://github.com/JerryJohnThomas/"></HoverText>
                     <HoverText text="Medium" hoverText="M3d1uW"to="https://medium.com/@jerryjohnthomas"></HoverText>
                     <HoverText text="Gmail" hoverText="gWaiL" mail="jerryjohnthomascareer@gmail.com"></HoverText>
